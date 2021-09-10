d3fdd1370fd674a4c3a2ea723e73b8fd7ca315c6
Gou Minghao <gmh2015@163.com>
2021-09-10 13:01:38 +0800

    Update create_container.sh

--- tools/create_container.sh
+++ tools/create_container.sh
@@ -5,5 +5,5 @@ sudo docker run -i -d --gpus all --name ocrtoc --network host \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -v $HOME/OCRTOC_software_package:/root/ocrtoc_ws/src \
         -v $HOME/Desktop/docker:/root/upload \
-        registry.cn-hangzhou.aliyuncs.com/ocrtoc2021/release:2.0
+        registry.cn-hangzhou.aliyuncs.com/ocrtoc2021/release:2.1
 sudo xhost +local:`docker inspect --format='{{ .Config.Hostname }}' ocrtoc`
